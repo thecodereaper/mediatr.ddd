84bf94c12202d13f7935ae9831a52c194a9f7c64
theCodeReaper <hello@theCodeReaper.com>
2020-05-17 13:31:49 +0200

    Added IPager.

---
+++ src/MediatR.DDD/IPager.cs
@@ -0,0 +1,11 @@
+﻿namespace MediatR.DDD
+{
+    public interface IPager
+    {
+        int Offset { get; }
+        int Limit { get; }
+        string SortBy { get; }
+        string SortOrder { get; }
+        string Filter { get; }
+    }
+}
